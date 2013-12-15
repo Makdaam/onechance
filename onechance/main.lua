eaf0a1d87e3d0e3f9a3510280781aa3751ed73f1
Leszek Jakubowski <makdaam@gmail.com>
2013-12-15 15:10:06 +0100

    all the shooting is done

--- onechance/main.lua
+++ onechance/main.lua
@@ -10,7 +10,13 @@ local gravity = 0.01
 local pc = {}
 local Collider = {}
 local controls = {}
+lastshot = 1000
 colup = 1
+redshootcycle = 10000000000
+defblueshootcycle = 10000000000
+blueshootcycle = 10000000000
+defblueshootcycle = 10000000000
+smokecycle = 10
 ship = {}
 trackedEntity = pc
 controls.vx = 0
@@ -40,6 +46,13 @@ camera.r = 0
 map = {}
 map.colliders = {}
 
+
+function getTan(x1,y1,x2,y2,len)
+    local xdiff = x2 - x1
+    local ydiff = y2 - y1
+    local z = math.sqrt((len*len)/((xdiff*xdiff) + (ydiff*ydiff)))
+    return {xdiff*z, ydiff*z}
+end
 
 local function Proxy(f)
   return setmetatable({}, {__index = function(self, k)
@@ -80,14 +93,108 @@ function removeLiving(e)
     else
         pc.bluekills = pc.bluekills + 1
     end
+    
+    if pc.bluekills < 1 then
+        defblueshootcycle = 100000000
+    elseif pc.bluekills < 5 then
+        defblueshootcycle = 500
+        if blueshootcycle > 500 then
+            blueshootcycle = 500
+        end
+    elseif pc.bluekills < 10 then
+        defblueshootcycle = 200
+        if blueshootcycle > 200 then
+            blueshootcycle = 200
+        end
+    elseif pc.bluekills < 15 then
+        defblueshootcycle = 100
+        if blueshootcycle > 100 then
+            blueshootcycle = 100
+        end        
+    elseif pc.bluekills < 20 then
+        defblueshootcycle = 50
+        if blueshootcycle > 50 then
+            blueshootcycle = 50
+        end
+    else
+        defblueshootcycle = 20
+        if blueshootcycle > 20 then
+            blueshootcycle = 20
+        end
+    end
+
+    if pc.redkills < 1 then
+        defredshootcycle = 100000000
+    elseif pc.redkills < 5 then
+        defredshootcycle = 500
+        if redshootcycle > 500 then
+            redshootcycle = 500
+        end
+    elseif pc.redkills < 10 then
+        defredshootcycle = 200
+        if redshootcycle > 200 then
+            redshootcycle = 200
+        end
+    elseif pc.redkills < 15 then
+        defredshootcycle = 100
+        if redshootcycle > 100 then
+            redshootcycle = 100
+        end        
+    elseif pc.redkills < 20 then
+        defredshootcycle = 50
+        if redshootcycle > 50 then
+            redshootcycle = 50
+        end
+    else
+        defredshootcycle = 20
+        if redshootcycle > 20 then
+            redshootcycle = 20
+        end
+    end
+
+
     pc.kills = pc.kills + 1
 end
 
-function setupCivilian(x,y, lx, rx, color)
-    local e = entity(x,y,0,0,32,32,false, color, Image.civilian, "civilian")
+function setupCivilian(x,y, lx, rx, side)
+    local e = entity(x,y,0,0,32,32,false, side, Image.civilian, "civilian")
+    Collider:addToGroup("alive",e.col)
+    e.side = side
+    if side == "red" then
+        e.color = {255,0,0}
+    elseif side == "blue" then
+        e.color = {0,0,255}
+    end
     e.hp = 100
     e.lx = lx
     e.rx = rx
+end
+
+function setupGuard(x,y, lx, rx, side)
+    local e = entity(x,y,0,0,32,32,false, side, Image.player, "guard")
+    Collider:addToGroup("alive",e.col)
+    e.side = side
+    if side == "red" then
+        e.color = {255,0,0}
+    elseif side == "blue" then
+        e.color = {0,0,255}
+    end
+    e.hp = 100
+    e.lx = lx
+    e.rx = rx
+end
+
+function setupGun(x, y, side)
+    local e = entity(x,y,0,0,64,64,false, color, Image.gun_base, "gun")
+    e.side = side
+    if side == "red" then
+        e.color = {255,0,0}
+    elseif side == "blue" then
+        e.color = {0,0,255}
+    end
+    Collider:addToGroup("alive",e.col)
+    Collider:addToGroup("bulletproof",e.col)
+    e.hp = 3000
 end
 
 function fireBullet(x,y,vx,vy,ttl)
@@ -101,6 +208,19 @@ function fireBullet(x,y,vx,vy,ttl)
     p.type = "bullet"
     p.col = Collider:addRectangle(p.x,p.y,3,3)
     p.col.parent = p
+    Collider:addToGroup("bulletproof",p.col)
+    table.insert(Projectiles,p)
+end
+
+function smokeTrace(x,y,ttl)
+    ttl = ttl or 100
+    local p = {}
+    p.x = x
+    p.y = y
+    p.vx = 0
+    p.vy = -0.01
+    p.ttl = ttl
+    p.type = "smoke"
     table.insert(Projectiles,p)
 end
 
@@ -118,7 +238,47 @@ function fireBomb(x,y,vx,vy,ttl)
     table.insert(Projectiles,p)
 end
 
+
+function doShoot(side)
+    for i,e in pairs(Entities) do
+        if e.side == side then
+            if e.type == "gun" then
+                if ((ship.x - e.x) * (ship.x - e.x)) + ((ship.y - e.y) * (ship.y - e.y)) < (500 * 500) then
+                    local tn = getTan(e.x+(e.sx/2),e.y+(e.sy/2),ship.x, ship.y, 3)
+                    fireBullet(e.x+(e.sx/2),e.y+(e.sy/2), tn[1], tn[2],200)
+                end
+            end
+            if e.type == "guard" and ((pc.x - e.x) * (pc.x - e.x)) < (200 * 200) and ((pc.x - e.x) * (pc.x - e.x)) > (32 * 32) and pc.gnd < 100 then
+                local lower = math.min(pc.x, e.x)
+                local higher = math.max(pc.x, e.x)
+                local shoot = true
+                for j,c in pairs(Entities) do
+                    if c.x > lower and c.x < higher then
+                        shoot = false
+                        break
+                    end
+                end
+                if shoot then
+                    if lower == pc.x and not e.flipx then
+                        e.vx = -e.vx
+                        e.flipx = true
+                    elseif lower == e.x and e.flipx then
+                        e.vx = -e.vx
+                        e.flipx = false
+                    end
+                    if e.flipx then
+                        fireBullet(e.x-4,e.y+13,e.vx-1,0,100)
+                    else
+                        fireBullet(e.x+34,e.y+13,e.vx+1,0,100)
+                    end
+                end
+            end
+        end
+    end
+end
+
 function updateProjectiles()
+    smokecycle = smokecycle - 1
     for i, p in pairs(Projectiles) do
         p.ttl = p.ttl - 1
         if p.ttl < 0 then
@@ -127,19 +287,33 @@ function updateProjectiles()
         else
             p.x = p.x + p.vx
             p.y = p.y + p.vy
-            p.col:moveTo(p.x+1,p.y+1)
+            if p.type == "bullet" or p.type == "bomb" then
+                p.col:moveTo(p.x+1,p.y+1)
+            end
         end
+        if smokecycle < 0 and p.type ~= "smoke" then
+            smokeTrace(p.x,p.y,100)
+        end
+    end        
+    if smokecycle < 0 then
+        smokecycle = 10
     end
 end
 
 function setupEntities()
-    for i=0,10 do
-        setupCivilian(100+math.random(2500),400,50,2900,{0,0,255})
+    for i=0,5 do
+        setupCivilian(100+math.random(2500),400,50,2900,"blue")
+        setupCivilian(4300+math.random(2500),400,4100,6900,"red")
     end
-    for i=0,10 do
-        setupCivilian(4300+math.random(2500),400,4100,6900,{255,0,0})
+    for i=0,5 do
+        setupGuard(100+math.random(2500),400,50,2900,"blue")
+        setupGuard(4300+math.random(2500),400,4100,6900,"red")
     end
-
+    for i=0,2 do
+        setupGun(100+math.random(2500),400,"blue")
+        setupGun(4300+math.random(2500),400,"red")
+    end
+    setupGun(200,400,"blue")
 end
 
 function drawProjectiles()
@@ -152,6 +326,11 @@ function drawProjectiles()
             love.graphics.setColor(255,128,0,255)
             love.graphics.rectangle("fill",p.x,p.y,7,7)
         end
+        if p.type == "smoke" then
+            love.graphics.setColor(0,0,0,64)
+            love.graphics.rectangle("fill",p.x,p.y,5,5)
+        end
+
     end
 end
 
@@ -176,13 +355,13 @@ local function drawHud()
     love.graphics.push()
     love.graphics.setColor(64,0,0,128)
     love.graphics.rectangle("fill",5,100,5,400)
-    love.graphics.setColor(255,0,0,128)
+    love.graphics.setColor(255,0,0,255)
     love.graphics.rectangle("fill",5,(100+(4*(100-pc.hp))),5,(4*pc.hp))
     love.graphics.setColor(0,0,0,128)
     --SHIP
     love.graphics.setColor(64,64,64,128)
     love.graphics.rectangle("fill",15,100,15,400)
-    love.graphics.setColor(230,230,230,128)
+    love.graphics.setColor(230,230,230,255)
     love.graphics.rectangle("fill",15,(100+(400-ship.hp)),15,(ship.hp))
     love.graphics.setColor(0,0,0,128)
 
@@ -190,7 +369,11 @@ local function drawHud()
     love.graphics.print(killstring, 6,6)
     love.graphics.setColor(255,255,0,255)
     love.graphics.print(killstring, 5,5)
+
+    love.graphics.print(blueshootcycle, 400, 5)
+
     love.graphics.pop()
+    
 end
 
 
@@ -243,12 +426,14 @@ function play:enter()
     ship = entity(200,400,0,0,128,64,false,{255,255,255,255},Image.ship_gear,"ship")
     ship.state = "gear"
     ship.hp = 400
+    ship.side = "white"
     ship.enterable = false
     
     setupEntities()
 end
 
 function play:update()
+    lastshot = lastshot + 1
     for i, e in pairs(Entities) do
         e.vy = e.vy + gravity
         e.x = e.x + e.vx
@@ -260,7 +445,7 @@ function play:update()
         elseif e.vx < 0 then
             e.flipx = true
         end
-        if e.type == "civilian" then
+        if e.type == "civilian" or e.type == "guard" then
             if e.vx == 0 then
                 e.vx = math.random() - 0.5
             else
@@ -268,6 +453,9 @@ function play:update()
                     e.vx = -e.vx
                 end
             end
+        end
+                
+        if e.type == "civilian" or e.type == "gun" or e.type == "guard" then
             if e.hp <= 0 then
                 removeLiving(e)
                 table.remove(Entities,i)
@@ -296,6 +484,13 @@ function play:update()
             pc.vy = 0
         end
     end
+    if ship.hp <= 0 then
+        removeLiving(ship)
+        ship.up = false
+        ship.img = Image.ship_dead
+        ship.enterable = false
+        trackedEntity = pc
+    end
     if pc.y>1000 or pc.hp <= 0 then
         Gamestate.switch(gameover)
     end
@@ -317,16 +512,32 @@ function play:update()
     --
     if colup <0 then
         Collider:update()
-        colup = 10
+        colup = 15
     end
     colup = colup -1
     camera:centerEntity(trackedEntity)
+    if redshootcycle < 0 then
+        redshootcycle = defredshootcycle
+        doShoot("red")
+    end;
+    redshootcycle = redshootcycle -1
+    if blueshootcycle < 0 then
+        blueshootcycle = defblueshootcycle
+        doShoot("blue")
+    end;
+    blueshootcycle = blueshootcycle -1
 end
 
 function play:draw()
     camera.set()
     map:draw()
     for i,e in pairs(Entities) do
+        if e.type == "gun" then
+            local tn = getTan(e.x+(e.sx/2), e.y+(e.sy/2), ship.x+(ship.sx/2), ship.y+(ship.sy/2),32)
+            print (tn[1], tn[2])
+            love.graphics.setColor(0,0,0,255)
+            love.graphics.line(e.x+(e.sx/2), e.y+(e.sy/2),e.x+(e.sx/2) + tn[1], e.y+(e.sy/2) + tn[2])
+        end
         love.graphics.setColor(e.color)
         if e.flipx then
             love.graphics.draw(e.img, e.x+e.sx, e.y,0,-1,1)
@@ -334,7 +545,6 @@ function play:draw()
             love.graphics.draw(e.img, e.x, e.y)
         end
     end
-    love.graphics.print(pc.gnd, 400, 300)
     drawProjectiles()
     camera.unset()
     drawHud()
@@ -350,10 +560,13 @@ function play:keypressed(key, code)
         elseif key == 'up' and pc.gnd < 100 then
             pc.vy = - 1.5
         elseif key == ' ' then
-            if pc.flipx then
-                fireBullet(pc.x-4,pc.y+13,pc.vx-1,0,100)
-            else
-                fireBullet(pc.x+34,pc.y+13,pc.vx+1,0,100)
+            if lastshot > 50 then
+                if pc.flipx then
+                    fireBullet(pc.x-4,pc.y+13,pc.vx-1,0,100)
+                else
+                    fireBullet(pc.x+34,pc.y+13,pc.vx+1,0,100)
+                end
+                lastshot = 0
             end
         elseif key == 'e' then
             if ship.enterable then
@@ -363,9 +576,9 @@ function play:keypressed(key, code)
     else
     --controlling ship
         if key =='left' then
-            controls.vx = -1
+            controls.vx = -3
         elseif key == 'right' then
-            controls.vx = 1
+            controls.vx = 3
         elseif key == 'up' and not ship.up then
             ship.up = true
             camera:scale(2,2)
@@ -375,14 +588,17 @@ function play:keypressed(key, code)
             ship.up = false
             camera:scale(0.5,0.5)
         elseif key == ' ' then
-            if ship.state == "gear" then
-                ship.state = "gun"
-                ship.img = Image.ship_gun
-            end
-            if ship.flipx then
-                fireBomb(ship.x+5,ship.y+70,ship.vx-1,1,500)
-            else
-                fireBomb(ship.x+105,ship.y+70,ship.vx+1,1,500)
+            if lastshot > 150 then
+                if ship.state == "gear" then
+                    ship.state = "gun"
+                    ship.img = Image.ship_gun
+                end
+                if ship.flipx then
+                    fireBomb(ship.x+5,ship.y+70,-1,1,400)
+                else
+                    fireBomb(ship.x+105,ship.y+70,1,1,400)
+                end
+                lastshot = 0
             end
         elseif key == 'e' then
                 trackedEntity = pc
@@ -460,22 +676,16 @@ function on_collision(dt, shape_a, shape_b, mtv_x, mtv_y)
     elseif t == 'bullet' then
         if e.hp then
             --an entity with health that we can decrement, yay!
-            e.hp = e.hp-5
+            e.hp = e.hp-20
             b.ttl = 0
         end
     elseif t == 'bomb' then
         if e.hp then
-            e.hp = e.hp - 10
+            e.hp = e.hp - 1000
             b.ttl = 0
         end
-        fireBullet(b.x-2,b.y,-0.1,0,50)
-        fireBullet(b.x-2,b.y-2,-0.1,-0.1,50)
-        fireBullet(b.x,b.y-2,0,-0.1,50)
-        fireBullet(b.x+2,b.y-2,0.1,-0.1,50)
-        fireBullet(b.x+2,b.y,0.1,0,50)
-        fireBullet(b.x+2,b.y+2,0.1,0.1,50)
-        fireBullet(b.x,b.y+2,0,0.1,50)
-        fireBullet(b.x-2,b.y+2,-0.1,0.1,50)
+        fireBullet(b.x-2,b.y,-0.2,0,200)
+        fireBullet(b.x+2,b.y,0.2,0,200)
     end
     --love.graphics.setBackgroundColor(255,0,0,255)
 end
