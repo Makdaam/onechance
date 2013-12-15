2097628ae1f5a7de0163ff0d0428c9584d527549
Leszek Jakubowski <makdaam@gmail.com>
2013-12-15 19:48:25 +0100

    distro fixed

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd onechance
+rm ../onechance.zip
 zip ../onechance.zip *
 cd ..
 mv onechance.zip onechance.love
@@ -8,5 +9,6 @@ cp onechance.love distro/onechance_linux.love
 
 cat windows_raw/love.exe onechance.love > distro/windows/onechance.exe
 cd distro
+rm onechance_windows.zip
 zip onechance_windows.zip windows/*
 
